316cb298a488efd7eff4e7241da93518c5e4495a
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-03-25 14:12:35 +0100

    Add depedency from core engine

---
+++ TestEngine/src/testengine/Main.java
@@ -0,0 +1,11 @@
+package testengine;
+
+import ch.sparkpudding.coreengine.CoreEngine;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CoreEngine core = new CoreEngine();
+	}
+
+}
