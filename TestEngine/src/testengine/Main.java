a83a19bac556100fb6cf238cf99be7ba322603e6
Pierre Bürki <pierre.burki@he-arc.ch>
2019-04-01 13:57:47 +0200

    Add Register

--- TestEngine/src/testengine/Main.java
+++ TestEngine/src/testengine/Main.java
@@ -1,10 +1,12 @@
 package testengine;
 
+import javax.swing.JPanel;
+
 import ch.sparkpudding.coreengine.CoreEngine;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new CoreEngine();
+		new CoreEngine(new JPanel());
 	}
 }
