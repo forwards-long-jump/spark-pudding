b019a4d31d9ca041070b943e6284ff8778cae656
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-04-01 14:42:16 +0200

    Add basic frame representation

--- SceneEditor/src/ch/sparkpudding/sceneeditor/FrameSceneEditor.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/FrameSceneEditor.java
@@ -1,12 +1,39 @@
 package ch.sparkpudding.sceneeditor;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+
+import ch.sparkpudding.sceneeditor.menu.MenuBar;
 
 public class FrameSceneEditor extends JFrame {
 
-	public FrameSceneEditor()
-	{
+	private static final String TITLE = "Scene Editor";
+	private static final int WIDTH = 1280;
+	private static final int HEIGHT = 720;
+
+	private MenuBar menuBar;
+	private BorderLayout borderLayout;
+
+	public FrameSceneEditor() {
+		init();
+		setupFrame();
+	}
+
+	private void init() {
+		borderLayout = new BorderLayout();
+		menuBar = new MenuBar();
+	}
+
+	private void setupFrame() {
+		setSize(WIDTH, HEIGHT);
+		setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle(TITLE);
+		setJMenuBar(menuBar);
+		setLayout(borderLayout);
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-	
+
 }
