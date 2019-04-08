c1daf5a22fbff30d360a1ef984c3bb9cc58e62aa
John Leuba <john.leuba@he-arc.ch>
2019-04-08 13:49:41 +0200

    Add basic structure of right side panel

--- SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelEntity.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelEntity.java
@@ -1,7 +1,12 @@
 package ch.sparkpudding.sceneeditor.panel;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class PanelEntity extends JPanel {
-
+	
+	
+	public PanelEntity() {
+		
+	}
 }
--- SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelSidebarRight.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelSidebarRight.java
@@ -1,7 +1,30 @@
 package ch.sparkpudding.sceneeditor.panel;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 public class PanelSidebarRight extends JPanel {
 
+	BoxLayout layout;
+
+	PanelScene panelScene;
+	PanelEntityTree panelEntityTree;
+	PanelEntity panelEntity;
+	PanelComponent panelComponent;
+
+	public PanelSidebarRight() {
+
+		panelScene = new PanelScene();
+		panelEntityTree = new PanelEntityTree();
+		panelEntity = new PanelEntity();
+		panelComponent = new PanelComponent();
+
+		layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+		setLayout(layout);
+
+		add(panelScene);
+		add(panelEntityTree);
+		add(panelEntity);
+		add(panelComponent);
+	}
 }
