b2a675fc706878e2fd5ddd5b14987363f5600f64
Pierre Bürki <pierre.burki@he-arc.ch>
2019-04-08 14:31:54 +0200

    Add parsing in CoreEngine constructor

--- CoreEngine/src/ch/sparkpudding/coreengine/CoreEngine.java
+++ CoreEngine/src/ch/sparkpudding/coreengine/CoreEngine.java
@@ -1,15 +1,30 @@
 package ch.sparkpudding.coreengine;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.swing.JPanel;
+import javax.xml.parsers.ParserConfigurationException;
 
+import org.xml.sax.SAXException;
+
+import ch.sparkpudding.coreengine.ecs.Component;
+import ch.sparkpudding.coreengine.ecs.Entity;
+import ch.sparkpudding.coreengine.ecs.Field;
 import ch.sparkpudding.coreengine.ecs.Scene;
-import ch.sparkpudding.coreengine.filereader.LelManager;
+import ch.sparkpudding.coreengine.filereader.LelFile;
+import ch.sparkpudding.coreengine.filereader.XMLParser;
 
+/**
+ * 
+ * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba
+ * 
+ */
 public class CoreEngine {
 
 	private double msPerUpdate = (1000 / 60);
@@ -18,8 +33,10 @@ public class CoreEngine {
 	private JPanel panel;
 	public Input input;
 	
-	private LelManager lelManager;
+	private LelFile lelFile;
 	
+	private Map<String, Component> componentTemplates;
+	private Map<String, Entity> entityTemplates;
 	private Map<String, Scene> scenes;
 	private Scene currentScene;
 	
@@ -30,18 +47,47 @@ public class CoreEngine {
 		this.panel = panel;
 		this.input = new Input(panel);
 		
-		this.lelManager = new LelManager(gameFolder);
-		
-		this.scenes = new HashMap<String, Scene>();
-		this.currentScene = null;
-		
-		this.systems = new ArrayList<System>();
-		this.renderSystem = null;
+		this.lelFile = new LelFile(gameFolder);
+		populateComponentTemplates();
+		populateEntityTemplates();
+		populateScenes();
+		loadSystems();
 		
 		
 		new Thread(() -> {
 			startGame();
 		}).start();
+	}
+
+	private void loadSystems() {
+		systems = new ArrayList<System>();
+		renderSystem = null;
+	}
+
+	private void populateScenes() {
+		scenes = new HashMap<String, Scene>();
+		currentScene = null;
+	}
+
+	private void populateEntityTemplates() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void populateComponentTemplates() throws ParserConfigurationException, SAXException, IOException {
+		componentTemplates = new HashMap<String, Component>();
+		for (File xmlFile : lelFile.getComponentsXML()) {
+			Component c = new Component(XMLParser.parse(xmlFile));
+			componentTemplates.put(c.getType(), c);
+		}
+		for (Entry<String, Component> components : componentTemplates.entrySet()) {
+			System.out.println(components.getKey() +" :");
+			for (Entry<String, Field> fields : components.getValue().getFields().entrySet())
+			{
+				System.out.println(" " + fields.getKey() + " -> " + fields.getValue().getValue());				
+			}
+			System.out.println();
+		}
 	}
 
 	private void startGame() {
