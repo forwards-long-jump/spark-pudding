4a77c1fceea2cf363cb4457f9f2de1e7260b7ede
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-04-08 13:27:26 +0200

    Add file and edit to the menu bar

--- SceneEditor/src/ch/sparkpudding/sceneeditor/menu/MenuBar.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/menu/MenuBar.java
@@ -2,6 +2,28 @@ package ch.sparkpudding.sceneeditor.menu;
 
 import javax.swing.JMenuBar;
 
+/**
+ * 
+ * @author Alexandre Bianchi
+ * 
+ */
 public class MenuBar extends JMenuBar {
 
+	private MenuFile menuFile;
+	private MenuEdit menuEdit;
+
+	public MenuBar() {
+		init();
+		addMenu();
+	}
+	
+	private void init() {
+		menuFile = new MenuFile();
+		menuEdit = new MenuEdit();
+	}
+
+	private void addMenu() {
+		add(menuFile);
+		add(menuEdit);
+	}
 }
