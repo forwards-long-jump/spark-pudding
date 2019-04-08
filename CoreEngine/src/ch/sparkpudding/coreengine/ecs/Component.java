74eee8eb5b44152e5c22f241afca29772360ac6a
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-04-08 14:58:50 +0200

    Add javadoc

--- CoreEngine/src/ch/sparkpudding/coreengine/ecs/Component.java
+++ CoreEngine/src/ch/sparkpudding/coreengine/ecs/Component.java
@@ -9,7 +9,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
- * 
+ * Represents settings (key values pairs) that can be attached to an entity
  * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba
  * 
  */
@@ -18,11 +18,19 @@ public class Component {
 	private String name;
 	private Map<String, Field> fields;
 
+	/**
+	 * Create an empty component
+	 * @param name A unique name per component
+	 */
 	public Component(String name) {
 		this.name = name;
 		this.fields = new HashMap<String, Field>();
 	}
 
+	/**
+	 * Create a component from a parsed XML Document and populate its fields
+	 * @param document A properly formated Document to get fields from
+	 */
 	public Component(Document document) {
 		this.fields = new HashMap<String, Field>();
 		this.name = document.getDocumentElement().getAttribute("name");
@@ -38,14 +46,26 @@ public class Component {
 		}
 	}
 
+	/**
+	 * Add a field to this component
+	 * @param field Field to add, name must not already exists
+	 */
 	public void addField(Field field) {
 		fields.put(field.getName(), field);
 	}
 	
+	/**
+	 * Fields getter
+	 * @return Map<String, Field> containing all fields
+	 */
 	public Map<String, Field> getFields() {
 		return fields;
 	}
 
+	/**
+	 * Name getter
+	 * @return name of the component
+	 */
 	public String getType() {
 		return name;
 	}
