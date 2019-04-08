78d33caf0996329d57c25b75b13b50128cfbb840
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-04-08 13:26:58 +0200

    Add basic menu representation for file and edit

--- SceneEditor/src/ch/sparkpudding/sceneeditor/menu/MenuEdit.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/menu/MenuEdit.java
@@ -1,7 +1,30 @@
 package ch.sparkpudding.sceneeditor.menu;
 
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.event.KeyEvent;
 
 public class MenuEdit extends JMenu {
+	
+	private JMenuItem itemUndo;
+	private JMenuItem itemRedo;
+	
+	public MenuEdit()
+	{
+		init();
+		addItem();
+	}
 
+	private void init() {
+		setText("Edit");
+		
+		itemUndo = new JMenuItem("Undo", KeyEvent.VK_1);
+		itemRedo = new JMenuItem("Redo");
+	}
+
+	private void addItem() {
+		add(itemUndo);
+		add(itemRedo);
+	}
 }
--- SceneEditor/src/ch/sparkpudding/sceneeditor/menu/MenuFile.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/menu/MenuFile.java
@@ -1,7 +1,31 @@
 package ch.sparkpudding.sceneeditor.menu;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MenuFile extends JMenu {
+	
+	private JMenuItem itemNew;
+	private JMenuItem itemSave;
+	
+	public MenuFile()
+	{
+		init();
+		addItem();
+	}
+
+	private void init() {
+		setText("File");
+		
+		itemNew = new JMenuItem("New", KeyEvent.VK_1);
+		itemSave = new JMenuItem("Save");
+	}
+
+	private void addItem() {
+		add(itemNew);
+		add(itemSave);
+	}
 
 }
