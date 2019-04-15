748d8f1b77c44bb148625cc5cc95cebd6bcc53d0
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-04-15 15:38:40 +0200

    Create basic file architecture
    Revert "Create basic file architecture"
    
    This reverts commit 68e35b7d3a34859038da44fe85f8a02b70e10589.
    
    Revert "Revert "Create basic file architecture""
    
    This reverts commit 84824846449211a8bb002e77dd839cbb05d586de.
    
    Add prefix to class

---
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/ecs/SEScene.java
@@ -0,0 +1,10 @@
+package ch.sparkpudding.sceneeditor.ecs;
+
+/**
+ * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba<br/>
+ *         Creation Date : 15 avr. 2019
+ *
+ */
+public class SEScene {
+
+}
