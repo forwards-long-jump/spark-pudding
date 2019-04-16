476e8144e06601c2896a719ec8344fb3a82d1ca2
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-04-16 17:04:06 +0200

    Add the correct actions for the edit menu

--- SceneEditor/src/ch/sparkpudding/sceneeditor/action/ActionTest.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/action/ActionTest.java
@@ -3,13 +3,22 @@ package ch.sparkpudding.sceneeditor.action;
 @SuppressWarnings("serial")
 public class ActionTest extends AbstractAction {
 
+	private int count = 0;
+	
 	public ActionTest(String name) {
 		super(name);
+	}
+	
+	@Override
+	public void undoAction() {
+		System.out.println("Undo dummy action");
+		count--;
 	}
 
 	@Override
 	public boolean doAction() {
-		System.out.println("Dummy action");
+		System.out.println("Dummy action" + count);
+		count++;
 		return true;
 	}
 
