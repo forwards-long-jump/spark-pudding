a84a2746bb8fe39d9c65e87ff9f8342beb070ef7
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-04-28 13:32:28 +0200

    Fix render system not using ipairs

--- TestEngine/src/testgame/systems/render.lua
+++ TestEngine/src/testgame/systems/render.lua
@@ -3,7 +3,7 @@ function getRequiredComponents()
 end
 
 function render(g)
-  for name, entity in pairs(entities) do
+  for i, entity in ipairs(entities) do
     pos = entity.position
     size = entity.size
     g:fillRect(pos.x, pos.y, size.width, size.height)
