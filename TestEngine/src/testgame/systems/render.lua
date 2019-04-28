935a037a4f7a82a6caf709f7a86408acbd1a6deb
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-04-28 14:49:36 +0200

    Add color API test

--- TestEngine/src/testgame/systems/render.lua
+++ TestEngine/src/testgame/systems/render.lua
@@ -1,11 +1,14 @@
 function getRequiredComponents()
-  return {"position", "size"}
+  return {"position", "size", "color"}
 end
 
 function render(g)
   for i, entity in ipairs(entities) do
     pos = entity.position
     size = entity.size
+    color = entity.color
+    
+    g:setColor(game.color:fromRGB(color.r, color.g, color.b))
     g:fillRect(pos.x, pos.y, size.width, size.height)
   end
 end
