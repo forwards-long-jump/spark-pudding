ed6cf913560ad673daa03ef3809908a0e737606b
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-04-28 18:45:30 +0200

    Add core API test

--- TestEngine/src/testgame/systems/render.lua
+++ TestEngine/src/testgame/systems/render.lua
@@ -11,4 +11,7 @@ function render(g)
     g:setColor(game.color:fromRGB(color.r, color.g, color.b))
     g:fillRect(pos.x, pos.y, size.width, size.height)
   end
+  
+  g:setColor(game.color:fromRGB(0, 0, 0))
+  g:drawString('tick: ' .. game.core:getTick(), 20, 20)
 end
