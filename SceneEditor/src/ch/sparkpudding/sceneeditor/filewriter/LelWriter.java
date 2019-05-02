af3c1317d94ff0e2b4b082fe766dc34595d572bb
John Leuba <john.leuba@he-arc.ch>
2019-05-02 16:40:41 +0200

    Fix xmlFromScene signature

--- SceneEditor/src/ch/sparkpudding/sceneeditor/filewriter/LelWriter.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/filewriter/LelWriter.java
@@ -19,12 +19,13 @@ public class LelWriter {
 	 * Save all the specified CoreEngine in the specified directory
 	 * 
 	 * @param coreEngine : The core engine who will be saved in the Lel folder.
-	 * @param directory : The Lel directory path
+	 * @param directory  : The Lel directory path
 	 * @throws IOException
 	 */
 	public void write(CoreEngine coreEngine, String directory) throws IOException {
-		// TODO : Adapt the code when we have a proper way to deal with default entities values and metadata in core engine.
-		
+		// TODO : Adapt the code when we have a proper way to deal with default entities
+		// values and metadata in core engine.
+
 		Map<String, Scene> scenes = coreEngine.getScenes();
 		// Check general architecture
 		String[] requiredFiles = { "", "assets", "assets/music", "assets/sounds", "assets/textures", "components",
@@ -38,7 +39,7 @@ public class LelWriter {
 		// Overwrite Scenes
 		for (Map.Entry<String, Scene> sceneEntry : scenes.entrySet()) {
 			File fScene = new File(directory + "/scenes/" + sceneEntry.getKey() + ".xml");
-			String xmlScene = xmlFromScene(sceneEntry.getValue(), coreEngine);
+			String xmlScene = xmlFromScene(sceneEntry.getValue());
 			Files.write(fScene.toPath(), xmlScene.getBytes());
 		}
 
@@ -64,25 +65,23 @@ public class LelWriter {
 	 * @param Scene to serialize
 	 * @return String to write in the xml file of the scene
 	 */
-	private String xmlFromScene(Scene scene, CoreEngine coreEngine) {
+	private String xmlFromScene(Scene scene) {
 		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 		xml += "<scene name=\"" + scene.getName() + "\">\n";
 		for (Entity entity : scene.getEntities()) {
 			xml += "\t<entity name=\"" + entity.getName() + "\" template=\"" + entity.getTemplate()
 					+ "\" z-index=\"1\">\n";
-			for(Component component : entity.getComponents().values())
-			{
+			for (Component component : entity.getComponents().values()) {
 				xml += "\t\t<component template=\"" + component.getTemplate() + "\">\n";
-				for(Field field : component.getFields().values())
-				{
+				for (Field field : component.getFields().values()) {
 					xml += "\t\t\t<field name=\"" + field.getName() + "\">" + field.getValue() + "</field>\n";
 				}
-				xml +="</component>\n";
+				xml += "</component>\n";
 			}
 			for (Entry<String, Component> component : Entity.getTemplates().get(entity.getTemplate())) {
-			    if (!entity.getComponents().keySet().contains(component.getValue().getName())) {
-			        xml += "<component name=\"" + component.getValue().getName() + "\" deleted=\"true\"/>";
-			    }
+				if (!entity.getComponents().keySet().contains(component.getValue().getName())) {
+					xml += "<component name=\"" + component.getValue().getName() + "\" deleted=\"true\"/>";
+				}
 			}
 			xml += "</entity>\n";
 		}
@@ -99,11 +98,9 @@ public class LelWriter {
 	private String xmlFromEntityTemplate(Entity entity) {
 		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 		xml += "<entity-template name=\"" + entity.getName() + "\">\n";
-		for(Component component : entity.getComponents().values())
-		{
+		for (Component component : entity.getComponents().values()) {
 			xml += "\t<component template=\"" + component.getName() + "\">\n";
-			for(Field field : component.getFields().values())
-			{
+			for (Field field : component.getFields().values()) {
 				xml += "\t\t<field name=\"" + field.getName() + "\">" + field.getValue() + "</field>\n";
 			}
 			xml += "\t</component>\n";
@@ -121,11 +118,11 @@ public class LelWriter {
 	private String xmlFromComponent(Component component) {
 		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 		xml += "<component name=\"" + component.getName() + "\">\n";
-		for(Field field : component.getFields().values())
-		{
-			xml += "\t<field  type=\"" + field.getType() +"\" name=\"" + field.getName() + "\">" + field.getValue() + "</field>\n";
+		for (Field field : component.getFields().values()) {
+			xml += "\t<field  type=\"" + field.getType() + "\" name=\"" + field.getName() + "\">" + field.getValue()
+					+ "</field>\n";
 		}
 		xml += "</component>";
-		return xml;	
+		return xml;
 	}
 }
