d0d6dac4cfd9f22e938e753a12e5a5c55ee5d037
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-05-03 11:04:15 +0200

    Add ImageLoader and ImageStorage to manage ressources

---
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/utils/ImageStorage.java
@@ -0,0 +1,19 @@
+package ch.sparkpudding.sceneeditor.utils;
+
+import javax.swing.ImageIcon;
+
+/**
+ * Class with static attributes to access basic images of the interface
+ * 
+ * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba<br/>
+ *         Creation Date : 3 mai 2019
+ *
+ */
+public class ImageStorage {
+
+	public static final ImageIcon PAUSE = ImageLoader.loadImage("pause.png");
+	public static final ImageIcon PLAY = ImageLoader.loadImage("play.png");
+	public static final ImageIcon STOP_DISABLED = ImageLoader.loadImage("stop_dis.png");
+	public static final ImageIcon STOP = ImageLoader.loadImage("stop.png");
+
+}
