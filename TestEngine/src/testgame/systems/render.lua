70939d29d31f86972ee08e74471b2c3172f81757
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-05-03 16:26:10 +0200

    Fix game not working with camera modifications

--- TestEngine/src/testgame/systems/render.lua
+++ TestEngine/src/testgame/systems/render.lua
@@ -1,5 +1,5 @@
 function getRequiredComponents()
-  return {players = {"position", "size", "color"}, texts = {"color", "position", "text"}}
+  return {players = {"position", "size", "color"}, texts = {"color", "position", "text"}, mice = {"position", "size", "color", "mouse"}}
 end
 
 function render(g)
@@ -12,12 +12,12 @@ function render(g)
     g:setColor(game.color:fromRGB(color.r, color.g, color.b))
     g:fillRect(pos.x, pos.y, size.width, size.height)
   end
-  
+
   g:setColor(game.color:fromRGBA(0, 0, 208, 5))
   g:fillRect(0, 0, game.core:getGameWidth(), game.core:getGameHeight())
 
-  
-  
+
+
   for i, entity in ipairs(texts) do
     pos = entity.position
     size = entity.size
@@ -26,12 +26,17 @@ function render(g)
     g:setColor(game.color:fromRGB(color.r, color.g, color.b))
     g:drawString(entity.text.value, pos.x, pos.y)
   end
-  
-  
-  g:setColor(game.color:fromRGB(253, 99, 128))
-  g:fillRect(620, 340, 40, 40)
 
-    game.camera:resetTransforms(g)
+  for i, entity in ipairs(mice) do
+    pos = entity.position
+    size = entity.size
+    color = entity.color
+
+    g:setColor(game.color:fromRGB(color.r, color.g, color.b))
+    g:fillRect(pos.x, pos.y, size.width, size.height)
+  end
+
+  game.camera:resetTransforms(g)
   g:drawString("FPS: " .. game.core:getFPS(), 20, 20)
-  
+
 end
