4baf6ba7f38c9484b4b12ca2b15d174d3cb77f46
Pierre Bürki <pierre.burki@he-arc.ch>
2019-05-04 17:29:15 +0200

    Change testgame to test texture and sound

--- TestEngine/src/testgame/systems/render.lua
+++ TestEngine/src/testgame/systems/render.lua
@@ -1,26 +1,24 @@
 function getRequiredComponents()
-  return {players = {"position", "size", "color"}, texts = {"color", "position", "text"}, mice = {"position", "size", "color", "mouse"}}
+  return {colored = {"position", "size", "color"},
+          texts = {"color", "position", "text"},
+          textured = {"position", "size", "texture"}}
 end
 
 function render(g)
   game.camera:applyTransforms(g)
-  local renderCount = 0
-  for i, entity in ipairs(players) do
+
+  -- Basic rectangles
+  for i, entity in ipairs(colored) do
     pos = entity.position
     size = entity.size
     color = entity.color
     if game.camera:isInView(pos.x, pos.y, size.width, size.height) then
-      renderCount = renderCount + 1
       g:setColor(game.color:fromRGB(color.r, color.g, color.b))
       g:fillRect(pos.x, pos.y, size.width, size.height)
     end
   end
 
-  g:setColor(game.color:fromRGBA(0, 0, 208, 5))
-  g:fillRect(0, 0, game.core:getGameWidth(), game.core:getGameHeight())
-
-
-
+  -- Text
   for i, entity in ipairs(texts) do
     pos = entity.position
     size = entity.size
@@ -29,17 +27,20 @@ function render(g)
     g:setColor(game.color:fromRGB(color.r, color.g, color.b))
     g:drawString(entity.text.value, pos.x, pos.y)
   end
-
-  for i, entity in ipairs(mice) do
+  
+  -- Rectangles with texture
+  for i, entity in ipairs(textured) do
     pos = entity.position
     size = entity.size
-    color = entity.color
-
-    g:setColor(game.color:fromRGB(color.r, color.g, color.b))
-    g:fillRect(pos.x, pos.y, size.width, size.height)
+    img = game.resources:getTexture(entity.texture.filename)
+    if game.camera:isInView(pos.x, pos.y, size.width, size.height) then
+      g:drawImage(img, pos.x, pos.y, size.width, size.height)
+    end
   end
 
   game.camera:resetTransforms(g)
+  
+  -- Optional : FPS counter
   g:drawString("FPS: " .. game.core:getFPS(), 20, 20)
 
 end
