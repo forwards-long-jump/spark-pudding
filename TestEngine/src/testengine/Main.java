783f42af8f1cafabc166444dce63bf8e5f35ab98
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-05-04 17:09:01 +0200

    Add F5 to reload systems live in test engine

--- TestEngine/src/testengine/Main.java
+++ TestEngine/src/testengine/Main.java
@@ -1,11 +1,15 @@
 package testengine;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
 import ch.sparkpudding.coreengine.CoreEngine;
 
 public class Main {
+	static CoreEngine ce = null;
 
 	public static void main(String[] args) {
 		JFrame jframe = new JFrame();
@@ -14,7 +18,6 @@ public class Main {
 		jframe.setLocationRelativeTo(null);
 		jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		CoreEngine ce = null;
 
 		try {
 			ce = new CoreEngine(Main.class.getResource("/testgame").getPath());
@@ -25,5 +28,15 @@ public class Main {
 		jframe.add(ce);
 
 		jframe.setVisible(true);
+		
+		// Refresh all systems when F5 is pressed
+		ce.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_F5) {
+					ce.scheduleSystemReloadFromDisk();
+				}
+			}
+		});	
 	}
 }
