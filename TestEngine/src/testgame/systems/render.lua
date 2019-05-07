f4a419bb42c2d8bc36255766abba61b1969bd7c6
Pierre Bürki <pierre.burki@he-arc.ch>
2019-05-07 15:48:49 +0200

    Add polygons in testgame, and changes draw calls from Lua

--- TestEngine/src/testgame/systems/render.lua
+++ TestEngine/src/testgame/systems/render.lua
@@ -1,22 +1,42 @@
 function getRequiredComponents()
   return {colored = {"position", "size", "color"},
           texts = {"color", "position", "text"},
-          textured = {"position", "size", "texture"}}
+          textured = {"position", "size", "texture"},
+          triangles = {"triangle", "size", "position", "color"}}
 end
 
-function render(g)
-  game.camera:applyTransforms(g)
+function render()
+  game.camera:applyTransforms(g:getContext())
   renderCount = 0
+
+  -- Triangles
+  for i, entity in ipairs(triangles) do
+    pos = entity.position
+    size = entity.size
+    points = entity.triangle
+    p1 = {pos.x + points.dx1, pos.y + points.dy1}
+    p2 = {pos.x + points.dx2, pos.y + points.dy2}
+    p3 = {pos.x + points.dx3, pos.y + points.dy3}
+    
+     if game.camera:isInView(pos.x, pos.y, size.width, size.height) then
+      renderCount = renderCount + 1
+      g:setColor(game.color:fromRGB(color.r, color.g, color.b))
+      g:fillPolygon({p1,p2,p3})
+    end
+  end
 
   -- Basic rectangles
   for i, entity in ipairs(colored) do
-    pos = entity.position
-    size = entity.size
-    color = entity.color
-    if game.camera:isInView(pos.x, pos.y, size.width, size.height) then
-      renderCount = renderCount + 1
-      g:setColor(game.color:fromRGB(color.r, color.g, color.b))
-      g:fillRect(pos.x, pos.y, size.width, size.height)
+    if entity.triangle == nil then
+      pos = entity.position
+      size = entity.size
+      color = entity.color
+  
+      if game.camera:isInView(pos.x, pos.y, size.width, size.height) then
+        renderCount = renderCount + 1
+        g:setColor(game.color:fromRGB(color.r, color.g, color.b))
+        g:fillRect(pos, size)
+      end
     end
   end
 
@@ -28,21 +48,20 @@ function render(g)
 
     renderCount = renderCount + 1
     g:setColor(game.color:fromRGB(color.r, color.g, color.b))
-    g:drawString(entity.text.value, pos.x, pos.y)
+    g:drawString(entity.text.value, pos)
   end
   
   -- Rectangles with texture
   for i, entity in ipairs(textured) do
     pos = entity.position
     size = entity.size
-    img = game.resources:getTexture(entity.texture.filename)
     if game.camera:isInView(pos.x, pos.y, size.width, size.height) then
       renderCount = renderCount + 1
-      g:drawImage(img, pos.x, pos.y, size.width, size.height)
+      g:drawImage(entity.texture.filename, pos, size)
     end
   end
 
-  game.camera:resetTransforms(g)
+  game.camera:resetTransforms(g:getContext())
 
   -- Optional : FPS counter and drawn entities counter
 
