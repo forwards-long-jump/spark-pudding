b3471b4020ece03d2a9d61be6eb54544b5d9beea
Pierre Bürki <pierre.burki@he-arc.ch>
2019-05-08 20:00:23 +0200

    Add mousemove and mousezoom in editingPause (but it's broken)

--- SceneEditor/resources/leleditor/systems/se-camera-controller.lua
+++ SceneEditor/resources/leleditor/systems/se-camera-controller.lua
@@ -8,29 +8,41 @@ function update()
   local dx = 0
   local dy = 0
   
-  --[[
-  for i = 0, 999 do
-    if game.input:isKeyDown(i) then
-      print(i)
-    end
-  end
-  --]]
+  
+--  for i = 0, 999 do
+--    if game.input:isMouseButtonDown(i) then
+--      print(i)
+--    end
+--  end
+  
   -- print(#entities)
-  if game.input:isKeyDown(38) then
-    dy = -speed
-  end
-  if game.input:isKeyDown(40) then
-    dy = speed
-  end
-  if game.input:isKeyDown(37) then
-    dx = -speed
-  end
-  if game.input:isKeyDown(39) then
-    dx = speed
-  end
+--  if game.input:isKeyDown(38) then
+--    dy = -speed
+--    game.camera:setMode("SMOOTH")
+--  end
+--  if game.input:isKeyDown(40) then
+--    dy = speed
+--    game.camera:setMode("SMOOTH")
+--  end
+--  if game.input:isKeyDown(37) then
+--    dx = -speed
+--    game.camera:setMode("SMOOTH")
+--  end
+--  if game.input:isKeyDown(39) then
+--    dx = speed
+--    game.camera:setMode("SMOOTH")
+--  end
 
-  game.camera:setTargetPosition(game.camera:getX() + dx, game.camera:getY() + dy)
+  if game.input:isMouseButtonDown(2) then
+    dx = game.input:getMouseDX()
+    dy = game.input:getMouseDY()
+  end
+    game.camera:setMode("NO_FOLLOW")
+  
+  game.camera:setPosition(game.camera:getX() - dx, game.camera:getY() - dy)
 
+  -- Commented out in case we still want to use buttons to zoom
+  --[[
   if game.input:isKeyDown(107) then
     scaling = scaling * 1.1
   end
@@ -38,7 +50,9 @@ function update()
   if game.input:isKeyDown(109) then
     scaling = scaling / 1.1
   end
-  
+  --]]
+  scaling = scaling * (1 - game.input:getMouseWheelRotation() * 0.3)
+  game.camera:setSmoothScaleSpeedCoeff(1)
   game.camera:setTargetScaling(scaling)
 end
 
