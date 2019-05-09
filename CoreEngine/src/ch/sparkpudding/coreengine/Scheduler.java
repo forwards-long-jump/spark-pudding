b6beba8d6ecf66690bd9bbab4ea956084ac85ace
Pierre Bürki <pierre.burki@he-arc.ch>
2019-05-09 18:00:02 +0200

    Add notifications to the Scheduler class

--- CoreEngine/src/ch/sparkpudding/coreengine/Scheduler.java
+++ CoreEngine/src/ch/sparkpudding/coreengine/Scheduler.java
@@ -11,31 +11,61 @@ import java.util.Map;
  * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba
  * 
  */
-public class TaskScheduler {
-	public enum Trigger{
+public class Scheduler {
+	public enum Trigger {
 		BEFORE_UPDATE, AFTER_UPDATE
 	};
-	
+
 	private Map<Trigger, List<Runnable>> tasks;
-	
+	private Map<Trigger, List<Runnable>> notifications;
+
 	/**
-	 * 
+	 * ctor
 	 */
-	public TaskScheduler() {
+	public Scheduler() {
 		tasks = new HashMap<Trigger, List<Runnable>>();
 		for (Trigger trig : Trigger.values()) {
 			tasks.put(trig, new ArrayList<Runnable>());
 		}
+		notifications = new HashMap<Trigger, List<Runnable>>();
+		for (Trigger trig : Trigger.values()) {
+			notifications.put(trig, new ArrayList<Runnable>());
+		}
 	}
-	
-	public void trigger(Trigger trig) {
-		for (Runnable task : tasks.get(trig)) {
+
+	/**
+	 * Runs all tasks and notifications associated with the given trigger
+	 * 
+	 * @param trigger Trigger
+	 */
+	public void trigger(Trigger trigger) {
+		for (Runnable task : tasks.get(trigger)) {
 			task.run();
 		}
-		tasks.get(trig).clear();
+		tasks.get(trigger).clear();
+
+		for (Runnable notif : notifications.get(trigger)) {
+			notif.run();
+		}
 	}
-	
+
+	/**
+	 * Add a task to be ran the next time the given trigger is activated
+	 * 
+	 * @param trigger  trigger that will run the task
+	 * @param runnable task
+	 */
 	public void schedule(Trigger trigger, Runnable runnable) {
 		tasks.get(trigger).add(runnable);
+	}
+
+	/**
+	 * Add a notification to be ran the next time the given trigger is activated
+	 * 
+	 * @param trigger  trigger that will run the notification
+	 * @param runnable notification
+	 */
+	public void notify(Trigger trigger, Runnable runnable) {
+		notifications.get(trigger).add(runnable);
 	}
 }
