3fdf3f4cf5a71c410ad001a3a97e9f6c941d16bc
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-05-09 18:56:24 +0200

    Implement setEnable recursively for PanelComponent

--- SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelComponent.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelComponent.java
@@ -1,6 +1,8 @@
 package ch.sparkpudding.sceneeditor.panel;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,5 +55,24 @@ public class PanelComponent extends JPanel {
 		removeAll();
 		add(new ComponentGenerator(entity.getComponents().values()), BorderLayout.CENTER);
 		revalidate();
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		List<Component> comps = getAllComponents(this);
+		for (Component comp : comps) {
+		       comp.setEnabled(enabled);
+		}
+	}
+	
+	public List<Component> getAllComponents(final Container c) {
+		Component[] comps = c.getComponents();
+		List<Component> compList = new ArrayList<Component>();
+		for (Component comp : comps) {
+			compList.add(comp);
+			if (comp instanceof Container)
+				compList.addAll(getAllComponents((Container) comp));
+		}
+		return compList;
 	}
 }
