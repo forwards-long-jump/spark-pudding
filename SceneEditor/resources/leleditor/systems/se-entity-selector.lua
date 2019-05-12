8ef47e9b95ae4333a612692a225b246341cc6738
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-05-12 15:47:34 +0200

    Change entity selection to allow selecting entity that are above others

--- SceneEditor/resources/leleditor/systems/se-entity-selector.lua
+++ SceneEditor/resources/leleditor/systems/se-entity-selector.lua
@@ -3,19 +3,31 @@ function getRequiredComponents()
 end
 
 local mouseClickedAtPreviousTick = false
+local previousSelectedIndex = -1
 function update()
 
   -- TODO: Handle z-index
+  local noEntitiesFound = true
   for i, entity in ipairs(entities) do
     if game.input:isMouseInRectangle(entity.position.x, entity.position.y, entity.size.width, entity.size.height) then
       entity._meta:addComponent("se-hover")
-      if mouseClickedAtPreviousTick then
+      if mouseClickedAtPreviousTick and previousSelectedIndex < i then
+        previousSelectedIndex = i
         mouseClickedAtPreviousTick = false
+        noEntitiesFound = false
         entity._meta:addComponent("se-selected")
       end
     else
       entity._meta:deleteComponent("se-hover")
     end
+  end
+
+  if noEntitiesFound and mouseClickedAtPreviousTick then
+    previousSelectedIndex = -1
+  end
+
+  if game.input:getMouseClickedX() ~= game.input:getMouseX() or game.input:getMouseClickedY() ~= game.input:getMouseY() then
+    previousSelectedIndex = -1
   end
 
   mouseClickedAtPreviousTick = false
