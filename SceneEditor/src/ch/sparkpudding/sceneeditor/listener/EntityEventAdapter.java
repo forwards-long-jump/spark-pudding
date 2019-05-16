96e2c00ea874e39f9d091bb4047afbf272489f75
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-05-16 13:11:10 +0200

    Fix missing javadoc

--- SceneEditor/src/ch/sparkpudding/sceneeditor/listener/EntityEventAdapter.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/listener/EntityEventAdapter.java
@@ -5,18 +5,21 @@ import java.util.Map;
 import ch.sparkpudding.sceneeditor.ecs.SEEntity;
 import ch.sparkpudding.sceneeditor.ecs.SEScene;
 
+/**
+ * Adapter for the entity event listener
+ * 
+ * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba<br/>
+ *         Creation Date : 16 May 2019
+ *
+ */
 public abstract class EntityEventAdapter implements EntityEventListener {
 
 	@Override
 	public void changeSelectedEntity(SEEntity entity) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void entityListChanged(Map<String, SEScene> seScenes) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
