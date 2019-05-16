b5ba25274f4815de964281b051bd00adad997c14
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-05-16 09:09:44 +0200

    Add better entity resizing and moving management

--- SceneEditor/resources/leleditor/systems/se-entity-selector.lua
+++ SceneEditor/resources/leleditor/systems/se-entity-selector.lua
@@ -6,6 +6,7 @@ end
 local draggingAction = -1
 local draggingStartEntitySize = {w = 0, h = 0}
 local draggingStartEntityPosition = {x = 0, y = 0}
+local draggingStartOffset = {x = 0, y = 0}
 
 function update()
   local cursorSize = 10 / game.camera:getScaling()
@@ -30,11 +31,20 @@ function update()
           draggingAction = i
           draggingStartEntitySize.w = entity.size.width
           draggingStartEntitySize.h = entity.size.height
-          draggingStartEntityPosition.x = entity.position.x
-          draggingStartEntityPosition.y = entity.position.y
+          draggingStartOffset.x = -entity.position.x + game.input:getMouseX()
+          draggingStartOffset.y = -entity.position.y + game.input:getMouseY()
+          draggingStartEntityPosition.x = game.input:getMouseX()
+          draggingStartEntityPosition.y = game.input:getMouseY()
         end
         break
       end
+    end
+
+    -- Allowing drag and dropping the whole entity
+    if game.input:isMouseInRectangle(entity.position.x, entity.position.y, entity.size.width, entity.size.height) and game.input:isMouseButtonDown(1) and draggingAction == -1 then
+      draggingAction = 4
+      draggingStartOffset.x = -entity.position.x + game.input:getMouseX()
+      draggingStartOffset.y = -entity.position.y + game.input:getMouseY()
     end
 
     -- Apply effects on live entity
@@ -43,50 +53,50 @@ function update()
       entity.size.width = draggingStartEntitySize.w + draggingStartEntityPosition.x - game.input:getMouseX()
       entity.size.height = draggingStartEntitySize.h + draggingStartEntityPosition.y - game.input:getMouseY()
 
-      entity.position.x = game.input:getMouseX()
-      entity.position.y = game.input:getMouseY()
+      entity.position.x = game.input:getMouseX() - draggingStartOffset.x
+      entity.position.y = game.input:getMouseY() - draggingStartOffset.y
     end
     if draggingAction == 1 then
       -- Middle top
       entity.size.height = draggingStartEntitySize.h + draggingStartEntityPosition.y - game.input:getMouseY()
-      entity.position.y = game.input:getMouseY()
+      entity.position.y = game.input:getMouseY()  - draggingStartOffset.y
     end
     if draggingAction == 2 then
       -- Top right
       entity.size.height = draggingStartEntitySize.h + draggingStartEntityPosition.y - game.input:getMouseY()
-      entity.position.y = game.input:getMouseY()
+      entity.position.y = game.input:getMouseY() - draggingStartOffset.y
 
       entity.size.width = -entity.position.x + game.input:getMouseX()
     end
     if draggingAction == 3 then
       -- Middle left
       entity.size.width = draggingStartEntitySize.w + draggingStartEntityPosition.x - game.input:getMouseX()
-      entity.position.x = game.input:getMouseX()
+      entity.position.x = game.input:getMouseX() - draggingStartOffset.x
     end
     if draggingAction == 4 then
       -- Move entity
-      entity.position.x = game.input:getMouseX() - (entity.size.width / 2)
-      entity.position.y = game.input:getMouseY() - (entity.size.height / 2)
+      entity.position.x = game.input:getMouseX() - draggingStartOffset.x
+      entity.position.y = game.input:getMouseY() - draggingStartOffset.y
     end
     if draggingAction == 5 then
       -- Middle right
-      entity.size.width = -entity.position.x + game.input:getMouseX()
+      entity.size.width = -entity.position.x + game.input:getMouseX() - draggingStartOffset.x + draggingStartEntitySize.w
     end
     if draggingAction == 6 then
       -- Bottom left
       entity.size.width = draggingStartEntitySize.w + draggingStartEntityPosition.x - game.input:getMouseX()
-      entity.position.x = game.input:getMouseX()
-      
-      entity.size.height = -entity.position.y + game.input:getMouseY()
+      entity.position.x = game.input:getMouseX() - draggingStartOffset.x
+
+      entity.size.height = -entity.position.y + game.input:getMouseY() - draggingStartOffset.y + draggingStartEntitySize.h
     end
     if draggingAction == 7 then
       -- Middle bottom
-      entity.size.height = -entity.position.y + game.input:getMouseY()
+      entity.size.height = -entity.position.y + game.input:getMouseY() - draggingStartOffset.y + draggingStartEntitySize.h
     end
     if draggingAction == 8 then
       -- Bottom right
-      entity.size.width = -entity.position.x + game.input:getMouseX()
-      entity.size.height = -entity.position.y + game.input:getMouseY()
+      entity.size.width = -entity.position.x + game.input:getMouseX() - draggingStartOffset.x + draggingStartEntitySize.w
+      entity.size.height = -entity.position.y + game.input:getMouseY() - draggingStartOffset.y + draggingStartEntitySize.h
     end
 
     entity.size.width = math.max(entity.size.width, 0)
