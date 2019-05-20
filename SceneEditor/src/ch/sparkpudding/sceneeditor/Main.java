e7ee9f76cd73ff2c15b8f55917e18d181abd6a05
John <john.leuba@he-arc.ch>
2019-05-20 13:42:33 +0200

    Add folder selection on startup, by cmd or modal

--- SceneEditor/src/ch/sparkpudding/sceneeditor/Main.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/Main.java
@@ -1,5 +1,6 @@
 package ch.sparkpudding.sceneeditor;
 
+import javax.swing.JFileChooser;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -29,10 +30,25 @@ public class Main {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+		String gamePath;
+		if (args.length > 0) {
+			gamePath = args[0];
+		} else {
+			JFileChooser fc = new JFileChooser();
+			// TODO :When passing to .lel files, uncomment the next two lines and remove the
+			// next uncommented line.
+			// fc.setAcceptAllFileFilterUsed(false);
+			// fc.addChoosableFileFilter(FileChooserUtils.getFilter());
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int returnVal = fc.showOpenDialog(null);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				gamePath = fc.getSelectedFile().getAbsolutePath();
+			} else
+				return;
+		}
+
 		try {
-			SceneEditor.coreEngine = new CoreEngine(Main.class.getResource("/emptygame").getPath(),
-					Main.class.getResource("/leleditor").getPath());
+			SceneEditor.coreEngine = new CoreEngine(gamePath, Main.class.getResource("/leleditor").getPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
