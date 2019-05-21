69b2196b6685390676632e2f785ac7d3d01baf68
Loïck Jeanneret <loick.jeanneret@he-arc.ch>
2019-05-21 09:32:17 +0200

    Add entity being centered when created

--- SceneEditor/src/ch/sparkpudding/sceneeditor/action/ActionAddEntity.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/action/ActionAddEntity.java
@@ -2,7 +2,9 @@ package ch.sparkpudding.sceneeditor.action;
 
 import javax.swing.SwingUtilities;
 
+import ch.sparkpudding.coreengine.Camera;
 import ch.sparkpudding.coreengine.Scheduler.Trigger;
+import ch.sparkpudding.coreengine.ecs.component.Component;
 import ch.sparkpudding.coreengine.ecs.entity.Entity;
 import ch.sparkpudding.sceneeditor.SceneEditor;
 
@@ -29,6 +31,22 @@ public class ActionAddEntity extends AbstractAction {
 		super("Add entity (" + name + ")");
 		this.newDefaultEntity = new Entity(Entity.getTemplates().get(templateName));
 		this.newDefaultEntity.setName(name);
+		
+		if (this.newDefaultEntity.hasComponent("position") && this.newDefaultEntity.hasComponent("size")) {
+			Camera camera = SceneEditor.coreEngine.getCamera();
+			Component size = this.newDefaultEntity.getComponents().get("size");
+
+			double width = size.getField("width").getDouble();
+			double height = size.getField("height").getDouble();
+
+			this.newDefaultEntity.getComponents().get("position").getField("x")
+					.setValue((camera.getPosition().getX() + SceneEditor.coreEngine.getGameWidth() / 2 - width / 2)
+							/ camera.getScaling());
+			this.newDefaultEntity.getComponents().get("position").getField("y")
+					.setValue((camera.getPosition().getY() + SceneEditor.coreEngine.getGameHeight() / 2 - height / 2)
+							/ camera.getScaling());
+		}
+
 		this.newLiveEntity = new Entity(this.newDefaultEntity);
 	}
 
