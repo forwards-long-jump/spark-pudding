7ebd7ccb4b0331bed074327ea2bb7c9432d899f6
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-05-28 16:27:49 +0200

    Implement systems editor

--- SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelEditor.java
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/panel/PanelEditor.java
@@ -1,20 +1,48 @@
 package ch.sparkpudding.sceneeditor.panel;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 
-import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.FileLocation;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rsyntaxtextarea.TextEditorPane;
 import org.fife.ui.rtextarea.RTextScrollPane;
+
+import ch.sparkpudding.coreengine.ecs.system.System;
+import ch.sparkpudding.sceneeditor.SceneEditor;
+import ch.sparkpudding.sceneeditor.action.ActionRemoveSystem;
+import ch.sparkpudding.sceneeditor.listener.SystemEventListener;
+import ch.sparkpudding.sceneeditor.panel.modal.ModalSystem;
+import ch.sparkpudding.sceneeditor.utils.ButtonTabComponent;
 
 /**
  * The panel for the lua editor
@@ -30,13 +58,12 @@ public class PanelEditor extends JPanel {
 
 	private JTabbedPane jTabbedPane;
 
-	private DefaultListModel<String> listModel;
+	private ListModel<System> listModel;
+	private List<String> nameSystems;
 	private JScrollPane listScroller;
-	private JList<String> jList;
+	private JList<System> jList;
 	private JPanel jPanelList;
 	private JPanel jPanelButton;
-
-	private RSyntaxTextArea rSyntaxTextArea;
 
 	private JButton buttonAdd;
 	private JButton buttonRemove;
@@ -47,31 +74,38 @@ public class PanelEditor extends JPanel {
 	public PanelEditor() {
 		init();
 		setupLayout();
+		addListener();
 	}
 
 	private void init() {
+		nameSystems = new ArrayList<String>();
 		jTabbedPane = new JTabbedPane();
 
 		jPanelList = new JPanel();
 		jPanelButton = new JPanel();
 
-		listModel = new DefaultListModel<String>();
-		jList = new JList<String>(listModel);
-		jList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		listModel = new DefaultListModel<System>();
+		jList = new JList<System>(listModel);
+		jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		jList.setLayoutOrientation(JList.VERTICAL);
 		jList.setVisibleRowCount(-1);
+		jList.setCellRenderer(new DefaultListCellRenderer() {
+			@Override
+			public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
+					boolean cellHasFocus) {
+				Component renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				if (renderer instanceof JLabel && value instanceof System) {
+					String name = ((System) value).getName();
+					// name = name.substring(name.lastIndexOf("."));
+					((JLabel) renderer).setText(name);
+				}
+				return renderer;
+			}
+		});
 
-		listModel.addElement("System1");
-		listModel.addElement("System2");
-		listModel.addElement("System3");
-		listModel.addElement("System4");
-		listModel.addElement("System5");
+		populateSystemList();
 
 		listScroller = new JScrollPane(jList);
-
-		rSyntaxTextArea = new RSyntaxTextArea(10, 60);
-		rSyntaxTextArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_LUA);
-		rSyntaxTextArea.setCodeFoldingEnabled(true);
 
 		buttonAdd = new JButton("+");
 		buttonRemove = new JButton("-");
@@ -88,9 +122,6 @@ public class PanelEditor extends JPanel {
 		listScroller.setMaximumSize(
 				new Dimension(PanelSidebarRight.BASIC_ELEMENT_WIDTH, PanelSidebarRight.BASIC_ELEMENT_HEIGHT));
 
-		RTextScrollPane sp = new RTextScrollPane(rSyntaxTextArea);
-		jTabbedPane.addTab("system", sp);
-
 		jPanelList.setLayout(new BorderLayout());
 		jPanelButton.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		jPanelButton.add(buttonAdd);
@@ -101,5 +132,165 @@ public class PanelEditor extends JPanel {
 
 		add(jTabbedPane, BorderLayout.CENTER);
 		add(jPanelList, BorderLayout.EAST);
+	}
+
+	/**
+	 * Add a tab for a system and verify if the tab already exist
+	 * 
+	 * @param system the system to add
+	 */
+	private void addTab(System system) {
+		if (jTabbedPane.indexOfTab(system.getName()) != -1) {
+			jTabbedPane.setSelectedIndex(jTabbedPane.indexOfTab(system.getName()));
+			return;
+		}
+
+		try {
+			ButtonTabComponent buttonTabComponent = new ButtonTabComponent(jTabbedPane);
+			
+			TextEditorPane editorPane = new TextEditorPane(TextEditorPane.INSERT_MODE, false,
+					FileLocation.create(system.getFilepath()));
+			editorPane.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_LUA);
+			editorPane.setCodeFoldingEnabled(true);
+			editorPane.setText(readFile(system.getFilepath()));
+			editorPane.setTabSize(2);
+			editorPane.convertSpacesToTabs();
+			editorPane.addCaretListener(new CaretListener() {
+				
+				@Override
+				public void caretUpdate(CaretEvent e) {
+					buttonTabComponent.setDirty(editorPane.isDirty());
+				}
+			});
+			editorPane.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if ((e.getKeyCode() == KeyEvent.VK_S) && (e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0) {
+						try {
+							editorPane.save();
+							buttonTabComponent.setDirty(editorPane.isDirty());
+							SceneEditor.coreEngine.reloadSystemsFromDisk();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				}
+			});
+
+			RTextScrollPane sp = new RTextScrollPane(editorPane);
+			jTabbedPane.addTab(system.getName(), sp);
+			jTabbedPane.setSelectedIndex(jTabbedPane.getTabCount() - 1);
+			jTabbedPane.setTabComponentAt(jTabbedPane.getTabCount() - 1, buttonTabComponent);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Read a file
+	 * 
+	 * @param filepath The path to the file
+	 * @return a {@code String} containing the content of the file
+	 */
+	private String readFile(String filepath) {
+		StringBuilder contentBuilder = new StringBuilder();
+
+		try (Stream<String> stream = Files.lines(Paths.get(filepath), StandardCharsets.UTF_8)) {
+			stream.forEach(s -> contentBuilder.append(s).append("\n"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return contentBuilder.toString();
+	}
+
+	/**
+	 * Add the different listener for each element of the panel
+	 */
+	private void addListener() {
+		SceneEditor.addSystemEventListener(new SystemEventListener() {
+
+			@Override
+			public void systemListChanged() {
+				populateSystemList();
+				findRemovedSystem();
+			}
+		});
+
+		jList.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent mouseEvent) {
+				if (mouseEvent.getSource() instanceof JList<?>) {
+
+					@SuppressWarnings("unchecked")
+					JList<System> list = (JList<System>) mouseEvent.getSource();
+					if (mouseEvent.getClickCount() == 2) {
+						int index = list.locationToIndex(mouseEvent.getPoint());
+						if (index >= 0) {
+							System system = list.getModel().getElementAt(index);
+							addTab(system);
+						}
+					}
+
+				}
+			}
+		});
+
+		buttonAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new ModalSystem();
+			}
+		});
+
+		buttonRemove.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (!jList.getSelectedValue().getName().equals("render.lua")) {
+						new ActionRemoveSystem(jList.getSelectedValue().getName()).actionPerformed(e);
+					} else {
+						JOptionPane.showMessageDialog(SceneEditor.frameSceneEditor,
+								"You can't remove the render system.", "Warning", JOptionPane.WARNING_MESSAGE);
+					}
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(SceneEditor.frameSceneEditor, "The system can't be removed.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+	}
+
+	/**
+	 * Find if a system has been removed and remove the associated pane
+	 */
+	private void findRemovedSystem() {
+		for (int i = 0; i < jTabbedPane.getTabCount(); i++) {
+			String title = jTabbedPane.getTitleAt(i);
+			if (!nameSystems.contains(title)) {
+				jTabbedPane.remove(i);
+
+				return;
+			}
+		}
+	}
+
+	/**
+	 * Populate the list of system
+	 */
+	private void populateSystemList() {
+		((DefaultListModel<System>) listModel).removeAllElements();
+		nameSystems.clear();
+
+		((DefaultListModel<System>) listModel).addElement(SceneEditor.coreEngine.getRenderSystems());
+		for (System system : SceneEditor.coreEngine.getSystems()) {
+			((DefaultListModel<System>) listModel).addElement(system);
+			nameSystems.add(system.getName());
+		}
+
+		revalidate();
 	}
 }
