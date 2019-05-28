4be763ac237d7126d35989d735dee39b2cd15a9b
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-05-28 16:21:26 +0200

    Add ActionAddSystem and ActionRemoveSystem

---
+++ SceneEditor/src/ch/sparkpudding/sceneeditor/action/ActionAddSystem.java
@@ -0,0 +1,91 @@
+package ch.sparkpudding.sceneeditor.action;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.SwingUtilities;
+
+import ch.sparkpudding.coreengine.Scheduler.Trigger;
+import ch.sparkpudding.sceneeditor.SceneEditor;
+
+/**
+ * The action to register the addition of a new system
+ * 
+ * @author Alexandre Bianchi, Pierre Bürki, Loïck Jeanneret, John Leuba<br/>
+ *         Creation Date : 28 May 2019
+ *
+ */
+@SuppressWarnings("serial")
+public class ActionAddSystem extends AbstractAction {
+
+	private String name;
+	private String content = "function getRequiredComponents()\n" + "	return {}\n" + "end\n" + "\n"
+			+ "function update()\n" + "	for i, entity in ipairs(entities) do\n" + "	end\n" + "end";
+
+	/**
+	 * ctor
+	 * 
+	 * @param name The name of the system
+	 */
+	public ActionAddSystem(String name) {
+		super("Add system (" + name + ")");
+		this.name = name;
+	}
+
+	@Override
+	public boolean doAction() {
+		SceneEditor.coreEngine.getScheduler().schedule(Trigger.GAME_LOOP_START, new Runnable() {
+
+			@Override
+			public void run() {
+				Path newFilePath = Paths.get(SceneEditor.coreEngine.getGameFolder() + "/systems/" + name + ".lua");
+				try {
+					Files.write(newFilePath, content.getBytes());
+					SceneEditor.coreEngine.reloadSystemsFromDisk();
+
+					SwingUtilities.invokeLater(new Runnable() {
+
+						@Override
+						public void run() {
+							SceneEditor.fireSystemListChanged();
+						}
+					});
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		return true;
+	}
+
+	@Override
+	public void undoAction() {
+		SceneEditor.coreEngine.getScheduler().schedule(Trigger.GAME_LOOP_START, new Runnable() {
+
+			@Override
+			public void run() {
+				Path newFilePath = Paths.get(SceneEditor.coreEngine.getGameFolder() + "/systems/" + name + ".lua");
+				try {
+					Files.delete(newFilePath);
+					SceneEditor.coreEngine.reloadSystemsFromDisk();
+
+					SwingUtilities.invokeLater(new Runnable() {
+
+						@Override
+						public void run() {
+							SceneEditor.fireSystemListChanged();
+						}
+					});
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
