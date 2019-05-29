5b6f19ad0681dabe162a376558dd725125ba86bc
Pierre Bürki <pierre.burki@he-arc.ch>
2019-05-29 10:21:01 +0200

    Change default component field names for less redundancy

--- SceneEditor/resources/basicgame/systems/b-controller-movement.lua
+++ SceneEditor/resources/basicgame/systems/b-controller-movement.lua
@@ -4,8 +4,8 @@ end
 
 function update()
   for i, entity in ipairs(accelerationEntities) do
-    entity.acceleration.movingUp = game.input:isKeyDown(game.input:keyFromString(entity.accelerationController.keyTop))
-    entity.acceleration.movingDown = game.input:isKeyDown(game.input:keyFromString(entity.accelerationController.keyBottom))
+    entity.acceleration.movingUp = game.input:isKeyDown(game.input:keyFromString(entity.accelerationController.keyUp))
+    entity.acceleration.movingDown = game.input:isKeyDown(game.input:keyFromString(entity.accelerationController.keyDown))
     entity.acceleration.movingLeft = game.input:isKeyDown(game.input:keyFromString(entity.accelerationController.keyLeft))
     entity.acceleration.movingRight = game.input:isKeyDown(game.input:keyFromString(entity.accelerationController.keyRight))
   end
